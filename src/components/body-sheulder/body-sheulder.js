8756b963152de10e51a87dfee03d4c2f309cc4d6
Kirill Menshikov <menshikov.kirill2604@yandex.ru>
2021-07-18 00:23:18 +0300

    Initial commit

--- src/components/body-sheulder/body-sheulder.js
+++ src/components/body-sheulder/body-sheulder.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Slider from '../slider';
 import ContentTable from '../content-table';
-import TaskLine from './task-line';
 
 import './body-sheulder.scss';
 
@@ -37,6 +36,41 @@ const WeekDayContent = ({data, isDayNow, name}) =>{
             </Slider>
         </div>
     );
+}
+
+const CircleTableElem = ({isActive}) =>{
+    return (
+        <span className={isActive? "circle crossed": "circle"}></span>
+    )
+}
+
+class TaskLine extends React.Component{
+    constructor(props){
+        super(props);
+        this.crosedLine = this.crosedLine.bind(this);
+
+        this.state = {isCrossedLineArray: new Array(this.props.data.length)}
+    }
+    crosedLine(ind){
+        let oldArray = this.state.isCrossedLineArray;
+        oldArray[ind] = !oldArray[ind];
+        this.setState({isCrossedLineArray: oldArray})
+    }
+    render(){
+        let list = this.props.data.map((elem, ind) =>{
+            return (<tr key={ind} className={this.state.isCrossedLine? "crossed": ""}>
+                <td><CircleTableElem onClick={(ind) => this.crosedLine(ind)} /></td>
+                <td>{elem.task}</td>
+                <td className="group">{elem.group}</td>
+                <td>{elem.time}</td>
+            </tr>)
+        });
+        return (
+           <>
+            {list}
+           </>
+        )
+    }
 }
 
 class BodySheulder extends React.Component{
